16fdf80ff50c03593611538bd16cafca7b2ed34e
tudor-m <tudor.morosan@yahoo.com>
2016-11-05 12:53:38 -0400

    rip, deep_xgb bug fixes

--- deep_xgb.R
+++ deep_xgb.R
@@ -1,4 +1,4 @@
-deep_xgb <- function(seed0=42,trainX,trainY,validX,validY,objective0,eta0,md0,mc0,subs0,cols0,base0,round0,seed1=42,pw1,objective1,eta1,md1,mc1,subs1,cols1,base1,round1)
+deep_xgb <- function(trainX,trainY,validX,validY,seed0,objective0,eta0,md0,mc0,subs0,cols0,base0,round0,seed1=42,pw1,objective1,eta1,md1,mc1,subs1,cols1,base1,round1)
 {
   set.seed(seed0)
   param0 <- list(objective = objective0, 
@@ -33,36 +33,32 @@ deep_xgb <- function(seed0=42,trainX,trainY,validX,validY,objective0,eta0,md0,mc
                  verbose = 2,
                  print.every = 10L,
                  maximize = FALSE)
-  dmodel1 <- xgb.DMatrix(as.matrix(trainX), label = trainY-pred_train_0)
-  dvalid1 <- xgb.DMatrix(as.matrix(validX), label = validY-pred_valid_0)      
+  err_train_0 = trainY - pred_train_0
+  err_valid_0 = validY - pred_valid_0
+  dtrain1 <- xgb.DMatrix(as.matrix(trainX)^pw1, label = err_train_0)
+  dvalid1 <- xgb.DMatrix(as.matrix(validX)^pw1, label = err_valid_0)      
   # generate model 1
-  xgb_model1 <- xgb.train(data = dmodel1, param, nrounds = round1, watchlist = list(mod = dtrain, val = dvalid))
+  xgb_model1 <- xgb.train(data = dtrain1, param, nrounds = round1, watchlist = list(mod = dtrain1, val = dvalid1))
       
-      pred11 = predict(xgb_model1,as.matrix(dt1[,-"Response",with=F])^pw)
-      pred21 = predict(xgb_model1,as.matrix(dt2[,-"Response",with=F])^pw)
-      #plot(pred10+pred11-dt1$Response)
-      #plot(pred20+pred21-dt2$Response)
-      ivec = vector();
-      errvec = vector();
-      for (i in seq(-3,3,0.1))
-      {ivec = append(ivec,i);
-      err = errMeasureRMSE(pred10+i*pred11,dt1$Response);
-      errvec = append(errvec,err)
-      #print(c(i,err))
-      }
-      imin1_1 = ivec[which.min(errvec)];
-      imin2_1 = mean(pred20)^pw/mean(pred10)^pw*imin1_1
-      #print(imin)
-      for (i in seq(-3,3,0.1)){
-        print(c(i,errMeasureRMSE(pred20+i*pred21,dt2$Response)))
-      }
-      plot(pred10-imin1_1*pred11-dt1$Response)
-      plot(pred20-imin2_1*pred21-dt2$Response)
-      print(errMeasureRMSE(pred10+imin1_1*pred11,dt1$Response))
-      print(errMeasureRMSE(pred20+imin2_1*pred21,dt2$Response))
-    }
+  pred_err_train_1 = predict(xgb_model1,as.matrix(trainX)^pw)
+  pred_err_valid_1 = predict(xgb_model1,as.matrix(validX)^pw)
+
+  ivec = vector();
+  errvec = vector();
+  for (i in seq(-3,3,0.1))
+  {
+    ivec = append(ivec,i);
+    err = errMeasureRMSE(pred_train_0 + i*pred_err_train_1,trainY);
+    errvec = append(errvec,err)
+  }
+  imin_train_1 = ivec[which.min(errvec)];
+  imin_valid_1 = (mean(pred_valid_0)^pw)/(mean(pred_train_0)^pw)*imin_train_1
+  for (i in seq(-3,3,0.1))
+  {
+    print(c(i,errMeasureRMSE(pred_valid_0+i*pred_err_valid_1,validY)))
+  }
+  pred_train_1 = pred_train_0 + imin_train_1*pred_err_train_1
+  pred_valid_1 = pred_valid_0 + imin_valid_1*pred_err_valid_1
   
-  
-  
-    
+  return(list(pred_train_0,pred_valid_0,pred_train_1,pred_valid_1))
 }
---
+++ xgb002.R
@@ -0,0 +1,61 @@
+library("xgboost")
+library("data.table")
+source("deep_xgb.R")
+
+errMeasureRMSE <- function(v1,v2)
+{
+  sqrt(mean((v1 - v2)^2))
+}
+
+nr1 = 1000
+nr2 = 1000
+dt1 = cbind(rnorm(nr1,1,0.2),rnorm(nr1,0,0.5),rnorm(nr1,2,0.83),rnorm(nr1,-1,0.1),rnorm(nr1,-5,1))
+dt1 = as.data.table(dt1)
+names(dt1) = c("a","b","c","d","e")
+dt1$Response = dt1[[1]]+dt1[[2]]^2+sin(dt1[[3]])+atan(dt1[[4]])+log(abs(dt1[[5]]))
+
+dt2 = cbind(rnorm(nr2,1,0.8),rnorm(nr2,0.,1),rnorm(nr2,2,0.25),rnorm(nr2,-1,0.5),rnorm(nr2,-5,0.5))
+dt2 = as.data.table(dt2)
+names(dt2) = c("a","b","c","d","e")
+dt2$Response = dt2[[1]]+dt2[[2]]^2+sin(dt2[[3]])+atan(dt2[[4]])+log(abs(dt2[[5]]))
+
+for (md in 26:26) for (mc in 6:6)
+{
+  print(c(is,md,mc))
+  set.seed(is)
+  #deep_xgb <- function(trainX,trainY,validX,validY,seed0,objective0,eta0,md0,mc0,subs0,cols0,base0,round0,seed1,pw1,objective1,eta1,md1,mc1,subs1,cols1,base1,round1)
+    
+  trainX = dt1[,-"Response",with=F]
+  trainY = dt1$Response
+  validX = dt2[,-"Response",with=F]
+  validY = dt2$Response
+  seed0 = 100
+  objective0 = "reg:linear"
+  eta0 = 0.01
+  md0 = 26
+  mc0 = 6
+  subs0 = 0.95
+  cols0 = 0.95
+  base0 = 0.50
+  round0 = 1000
+  pw1 = 5
+  seed1 = 100
+  objective1 = "reg:linear"
+  eta1 = 0.01
+  md1 = 2
+  mc1 = 1
+  subs1 = 0.95
+  cols1 = 0.95
+  base1 = 0.0
+  round1 = 1000
+
+  ret = deep_xgb(trainX,trainY,validX,validY,seed0,objective0,eta0,md0,mc0,subs0,cols0,base0,round0,seed1,pw1,objective1,eta1,md1,mc1,subs1,cols1,base1,round1)
+
+}
+
+
+print(errMeasureRMSE(ret[[1]],dt1$Response))
+print(errMeasureRMSE(ret[[2]],dt2$Response))
+print(errMeasureRMSE(ret[[3]],dt1$Response))
+print(errMeasureRMSE(ret[[4]],dt2$Response))
+
